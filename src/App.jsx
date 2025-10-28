a858050d32a1d2480a744afd9b87c6659f8b4961
Janver <caetachyon@protonmail.com>
2025-10-28 17:17:25 +0800

    Created BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { BrowserRouter } from 'react-router'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+
 
 export default function App() {
-  return (
-    <>
-    </>
+  return (  
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
   )
 }
