4e6aac15aa7e171b9c0dd23e3220c0b543f7aa53
DaddyJiAr <santiagojr0212@gmail.com>
2025-10-28 15:46:35 +0800

    navbar at footer

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,22 @@
+import { NavLink } from 'react-router'
 import labableLogo from '../../assets/logo.png'
+import './nav-bar.css'
 export default  function NavBar(){
-
     return(
         <>
+            <div className="nav-bar-container">
+                <img src={labableLogo} alt="" className='logo'/>
+                <div className="links">
+                    <NavLink to='/' className='nav-link'>Home</NavLink>
+                    <NavLink to='/' className='nav-link'>About us</NavLink>
+                    <NavLink to='/' className='nav-link'>Contact</NavLink>
+                    <NavLink to='/' className='nav-link'>Create order</NavLink>
+                </div>
+                <div className="login-signup">
+                    <button className='login'>Login</button>
+                    <button className='signup'>Signup</button>
+                </div>
+            </div>
         </>
     )
 }
