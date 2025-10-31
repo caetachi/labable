5d4e9e239bd953f066b8818945aaadd5ec46bf05
DaddyJiAr <santiagojr0212@gmail.com>
2025-10-31 14:37:09 +0800

    inayos ko firebase

--- src/firebase.js
+++ src/firebase.js
@@ -18,5 +18,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
