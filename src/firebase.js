132d54625dcc7dd2c9c0069da83d86f1128c94d9
tachaeyon <caetachyon@protonmail.com>
2025-11-02 06:49:10 +0800

    Remove API key from firebase.js
    Removed API key from Firebase configuration for security.

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADjFtpHYaScp27kFO094K31F4iy8m-GOQ",
+  apiKey: "",
   authDomain: "labable-55008.firebaseapp.com",
   databaseURL: "https://labable-55008-default-rtdb.firebaseio.com",
   projectId: "labable-55008",
@@ -16,7 +12,6 @@ const firebaseConfig = {
   appId: "1:282718692559:web:803c185513913d6faa83ba"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
